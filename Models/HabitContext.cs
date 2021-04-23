519403e47ef7dd81127f9f49dfaa807aefc3aa10
idesignerse <heberandrade@gmail.com>
2021-04-23 09:05:11 +0200

    Starter
    You can find the Idea of the project in a text file called: Final Project First thoughts.

---
+++ Models/HabitContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Habits.Models
+{
+    public class HabitContext : DbContext
+    {
+        public DbSet<Habit> Habits { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            DbContextOptionsBuilder dbContextOptionsBuilder = optionsBuilder.UseSqlServer(
+            @"Data Source = S5D011\SQLEXPRESS; Initial Catalog = HabitCatalog;" +
+            " Integrated Security = True; Connect Timeout = 30; Encrypt = False;" +
+            " TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"
+        );
+        }
+
+    }
+}
